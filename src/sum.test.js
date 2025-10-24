206eae0e51a657ad8fa7f4e7d5fc1ccbfc7d4a07
Moeez Abid <moeezabidd@gmail.com>
2025-10-24 10:22:21 +0500

    updated test file

--- src/sum.test.js
+++ src/sum.test.js
@@ -1,7 +1,30 @@
+const request = require('supertest');
+const { app, resetStore } = require('./sum');
 
+beforeEach(() => {
+  resetStore();
+});
 
-const sum = require('./sum');
+test('POST /add returns sum', async () => {
+  const res = await request(app).post('/add').send({ a: 2, b: 3 });
+  expect(res.status).toBe(200);
+  expect(res.body.result).toBe(5);
+});
 
-test('adds 1+2 to equal 3', ()=>{
-	expect(sum(1,2)).toBe(3);
+test('POST /multiply returns product', async () => {
+  const res = await request(app).post('/multiply').send({ a: 3, b: 4 });
+  expect(res.status).toBe(200);
+  expect(res.body.result).toBe(12);
+});
+
+test('create calculation and delete it', async () => {
+  const create = await request(app)
+    .post('/calculations')
+    .send({ op: 'add', a: 4, b: 6 });
+  expect(create.status).toBe(201);
+  expect(create.body).toHaveProperty('id');
+
+  const id = create.body.id;
+  const del = await request(app).delete(`/calculations/${id}`);
+  expect(del.status).toBe(204);
 });
